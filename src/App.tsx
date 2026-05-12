7853cb9c9a82240b6e51803fcd7f3ce8b9be7a42
Gerard Bataller <gebaca01@gmail.com>
2026-05-12 19:52:47 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className='bg-blue-700'>
+      <h1 className='text-3xl font-bold underline text-red-500'>
+        ¿Funciona Tailwind?
+      </h1>
+    </div>
+  );
+}
+export default App;
