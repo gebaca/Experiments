0859a6606b1b1675a6ddab52aa3ec682701670b7
Gerard Bataller <gebaca01@gmail.com>
2026-05-13 12:04:15 +0200

    a

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,7 @@ export function NotFound() {
   return (
     <div className='w-full h-screen bg-black flex flex-col items-center justify-center text-white'>
       <h1 className='text-4xl font-bold'>404</h1>
-      <p className='text-gray-400 mt-2'>Experimento no encontrado</p>
+      <p className='text-gray-400 mt-2'>Experiment not found</p>
     </div>
   );
 }
